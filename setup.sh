2860baa79a0b72714d3ef3c2de2207c44849ad96
roberto.soares <roberto.soares@mac179057.glb.com>
2012-12-11 17:36:12 -0200

    add vim-powerline

--- setup.sh
+++ setup.sh
@@ -3,4 +3,8 @@
 echo "source ~/.vim/vimrc" > ~/.vimrc
 echo "source ~/.vim/gvimrc" > ~/.gvimrc
 
+open fonts/SourceCodePro-Light-Powerline.otf
+open fonts/SourceCodePro-Regular-Powerline.otf
+open fonts/SourceCodePro-Bold-Powerline.otf
+
 exit 0
