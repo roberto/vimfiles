29fb8c8cab05e0be2fe7013e48bd793dba43e41b
Roberto Soares <roberto.tech@gmail.com>
2014-03-24 14:08:52 -0300

    changed font

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 echo "source ~/.vim/vimrc" > ~/.vimrc
 echo "source ~/.vim/gvimrc" > ~/.gvimrc
 
-open fonts/SourceCodePro-Light-Powerline.otf
-open fonts/SourceCodePro-Regular-Powerline.otf
-open fonts/SourceCodePro-Bold-Powerline.otf
+open fonts/Inconsolata-dz\ for\ Powerline.otf
 
 exit 0
