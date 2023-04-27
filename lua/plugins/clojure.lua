72c48142f0c44c3a89aabd8f458a35190e89c1e9
Roberto Soares <roberto.tech@gmail.com>
2023-04-27 15:40:13 -0300

    Disable auto repl and its terminal

--- lua/plugins/clojure.lua
+++ lua/plugins/clojure.lua
@@ -4,6 +4,8 @@ return {
     ft = "clojure",
     init = function()
       vim.g["conjure#mapping#prefix"] = ","
+      vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
+      vim.g["conjure#client#clojure#nrepl#connection#auto_repl#hidden"] = false
     end,
   },
   {
