63ad768dfc5b3074210879363660b5edcd319016
Roberto Soares <roberto.tech@gmail.com>
2023-06-08 22:37:12 -0300

    disable diagnostic for conjure log
    it was filling up the log with error messages

--- lua/plugins/clojure.lua
+++ lua/plugins/clojure.lua
@@ -6,6 +6,10 @@ return {
       vim.g["conjure#mapping#prefix"] = ","
       vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
       vim.g["conjure#client#clojure#nrepl#connection#auto_repl#hidden"] = false
+      vim.api.nvim_create_autocmd(
+        "BufNewFile",
+        { pattern = "conjure-log-*", command = "lua vim.diagnostic.disable(0)" }
+      )
     end,
   },
   {
