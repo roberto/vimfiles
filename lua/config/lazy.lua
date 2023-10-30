c8bc3a4565156aa8d218d482e57e74ced97b9985
Roberto Soares <roberto.tech@gmail.com>
2023-10-30 12:34:29 -0300

    github copilot cmp setup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,10 +15,10 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "saadparwaiz1/cmp_luasnip",
+  },
+  {
     "L3MON4D3/LuaSnip",
     keys = function()
       return {}
