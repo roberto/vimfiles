30f43bccf4aba1aaac9c5ba22c80cf31bbb488c9
Roberto Soares <roberto.tech@gmail.com>
2023-11-20 15:58:49 -0300

    Improve indent keymap
    this way it will be repeatable

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,11 @@
 local mapkey = require("util.keymapper").mapkey
+local keymap = vim.keymap
 
 -- Directory Navigation
 
 mapkey("<leader>ee", ":NvimTreeToggle<CR>", {desc = "Toggle Explorer"})
+
+-- Indenting
+
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
