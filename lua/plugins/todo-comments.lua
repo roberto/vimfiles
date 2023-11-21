bca00b32b835c8f3bd328d5dfde320378c0a2483
Roberto Soares <roberto.tech@gmail.com>
2023-11-20 22:33:50 -0300

    Add plugin todo-comments

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+    -- TODO: keymap
+    -- NOTE: https://github.com/folke/todo-comments.nvim#jumping
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+}
+
