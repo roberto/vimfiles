ac981808a077773e30c46cc23f0e789b87b70f80
Roberto Soares <roberto.tech@gmail.com>
2023-11-25 14:21:03 -0300

    Change lspsaga action keys

--- lua/plugins/lspsaga-nvim.lua
+++ lua/plugins/lspsaga-nvim.lua
@@ -5,17 +5,18 @@ return {
     require("lspsaga").setup({
       -- keybinds for navigation in lspsaga window
       move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-      -- use enter to open file with finder
-      finder_action_keys = {
-        open = "<CR>",
+      definition = {
+        keys = {
+          edit = "o",
+        },
       },
-      -- use enter to open file with definition preview
-      definition_action_keys = {
-        edit = "<CR>",
+      lightbulb = {
+        enable = false,
       },
     })
   end,
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
   },
 }
