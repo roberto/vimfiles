2bd5e1d9bcaf313ba0ca16677a5641dd563d84f3
Roberto Soares <roberto.tech@gmail.com>
2023-11-25 22:33:07 -0300

    Add clojure setup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins.clojure" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -39,7 +40,7 @@ require("lazy").setup({
         "tarPlugin",
         "tohtml",
         "tutor",
-        "zipPlugin",
+        -- "zipPlugin", (used by clj-lsp go-to-definition)
       },
     },
   },
---
+++ lua/plugins/clojure/conjure.lua
@@ -0,0 +1,11 @@
+return {
+  "Olical/conjure",
+  ft = "clojure",
+  init = function()
+    vim.g["conjure#mapping#prefix"] = ","
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#hidden"] = false
+    vim.g["conjure#mapping#doc_word"] = false
+    vim.api.nvim_create_autocmd("BufNewFile", { pattern = "conjure-log-*", command = "lua vim.diagnostic.disable(0)" })
+  end,
+}
