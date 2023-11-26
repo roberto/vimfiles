233b3f14c5e463d3ae1763f3c6a18cd722ddac0a
Roberto Soares <roberto.tech@gmail.com>
2023-11-26 04:10:38 -0300

    Add dart setup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
     { import = "plugins.clojure" },
+    { import = "plugins.dart" },
     { import = "plugins.extras" },
   },
   defaults = {
