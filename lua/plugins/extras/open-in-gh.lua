edce1d42c2a0c37ac6d53e16ab04e025d5c053d4
Roberto Soares <roberto.tech@gmail.com>
2023-11-26 00:36:55 -0300

    Add plugin open in github

---
+++ lua/plugins/extras/open-in-gh.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "almo7aya/openingh.nvim",
+    keys = {
+      { "<leader>go", "<cmd>OpenInGHFileLines <cr>", mode = { "n", "v" }, desc = "Open file in Github" },
+    },
+  },
+}
