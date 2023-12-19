c3c19dc249f7a7de60e550a335e69927b3a241bb
Roberto Soares <roberto.tech@gmail.com>
2023-12-19 11:40:36 -0300

    Add test form names

--- lua/plugins/clojure/conjure.lua
+++ lua/plugins/clojure/conjure.lua
@@ -5,6 +5,8 @@ return {
     vim.g["conjure#mapping#prefix"] = ","
     vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
     vim.g["conjure#client#clojure#nrepl#connection#auto_repl#hidden"] = false
+    vim.g["conjure#client#clojure#nrepl#test#current_form_names"] =
+      { "deftest", "defflow", "defflow-i18n", "defspec", "facts" }
     vim.g["conjure#mapping#doc_word"] = false
     vim.api.nvim_create_autocmd("BufNewFile", { pattern = "conjure-log-*", command = "lua vim.diagnostic.disable(0)" })
   end,
